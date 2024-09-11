c8f110ba8eb9e1f85bc1887afbeef30378c94f73
rajendrachy <chyrajendra32@gmail.com>
2024-09-11 18:16:54 +0530

    NUMPY

---
+++ NUMPY/PART 1/example.py
@@ -0,0 +1,14 @@
+# import numpy as np
+# x = np.array([1,2,3,4])
+# print(x)
+# print(type(x))  # checking the type
+
+# y = [1,2,3,4]
+# print(y)
+# print(type(y))  # checking the type
+
+
+import numpy
+arr = numpy.array([100, 20, 40, 85, 90])
+print(arr)
+print(type(arr))
---
+++ NUMPY/PART 1/r.py
@@ -0,0 +1,5 @@
+import random
+a = random.randint(5, 10)
+print(a)
+
+
